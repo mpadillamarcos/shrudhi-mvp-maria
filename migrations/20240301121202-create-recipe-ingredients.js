d724fdc3a8e4852278815919072a358dfb10c36d
Maria Padilla <maria.pad.mar@gmail.com>
2024-03-01 13:19:05 +0100

    Create database with Sequelize

---
+++ migrations/20240301121202-create-recipe-ingredients.js
@@ -0,0 +1,45 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return await queryInterface.createTable("RecipeIngredients", {
+      RecipeId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "recipes",
+          key: "id",
+        },
+        allowNull: false,
+      },
+      IngredientId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "ingredients",
+          key: "id",
+        },
+        allowNull: false,
+      },
+      Quantity: {
+        type: Sequelize.DECIMAL,
+        allowNull: true,
+      },
+      Units: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    return await queryInterface.dropTable("RecipeIngredients");
+  },
+};
