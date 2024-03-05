2be659390762296b9f330403a13799aa0a453f7f
Maria Padilla <maria.pad.mar@gmail.com>
2024-03-05 14:38:29 +0100

    Clean FilterRecipies code

--- client/src/pages/FilterRecipes.jsx
+++ client/src/pages/FilterRecipes.jsx
@@ -1,26 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// /*
-// Get Recipes Page:
-// 1. Need a form.
-// 2. Dropdown to select ingredients name with the id. (router.get('/ingredients').
-//     - select button
-//     - list of all recipes (name)
-// 3. Button to submit the selected ingredients which will update the post('/generate-recipe'.
-//     - req.body (ingredients)
-
-// Section to display generated recipe
-
-// */
-// // Form
-// // useEffect
-
-// function GetRecipes() {
-//   return (
-//     <div>GetRecipes</div>
-//   )
-// }
-// export default GetRecipes;
-
 import React, { useState, useEffect } from "react";
 import Form from "react-bootstrap/Form";
 import { Typeahead } from "react-bootstrap-typeahead";
@@ -40,17 +17,14 @@ function FilterRecipes() {
     const ingredientInformation = [...ingredientFields];
     ingredientInformation[index] = selected;
     setIngredientFields(ingredientInformation);
-    console.log(ingredientInformation);
+
     const ingredientName = ingredientInformation[index][0];
-    console.log(ingredientName);
     const ingredientID = ingredients.find(
       (ingredient) => ingredient.name === ingredientName
     ).id;
-    console.log(ingredientID);
     setSelectedIngredients([...selectedIngredients, ingredientID]);
   };
 
-  // Effect hook to fetch ingredients data from the backend when component mounts
   useEffect(() => {
     fetchIngredients();
   }, []);
@@ -77,21 +51,6 @@ function FilterRecipes() {
     }
   };
 
-  // Function to handle selection of ingredients
-  //  This function is called when the user selects ingredients from the dropdown.
-  // It extracts the values of the selected options and updates the selectedIngredients state accordingly.
-  // const handleIngredientSelect = (e) => {
-  //   const selectedValues = Array.from(
-  //     e.target.selectedOptions,
-  //     (option) => option.value
-  //   );
-  //   setSelectedIngredients(selectedValues);
-  // };
-
-  // Function to submit selected ingredients and generate recipe
-  //  This function is called when the user clicks the "Generate Recipe" button.
-  // It sends a POST request to /api/generate-recipe with the selected ingredients in the request body.
-  // Upon receiving a successful response, it updates the generatedRecipes state with the generated recipes.
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -107,8 +66,7 @@ function FilterRecipes() {
       });
       if (response.ok) {
         const data = await response.json();
-        console.log(data);
-        setFilteredRecipes(data); // Update state with generated recipe
+        setFilteredRecipes(data);
       } else {
         console.error("Error generating recipe");
       }
@@ -117,17 +75,9 @@ function FilterRecipes() {
     }
   };
 
-  // The component renders a form with a dropdown menu allowing users to select multiple ingredients.
-  //It renders a button labeled "Generate Recipe" which triggers the handleSubmit function when clicked.
-  //If there are generated recipes (generatedRecipes array is not empty), it renders them in a section titled "Generated Recipes".
-  //The generated recipes are displayed with their names and instructions.
-  //{generatedRecipes.length !== 0 && (...)}: This is a conditional rendering in JSX. It checks whether the length of the generatedRecipes array is not equal to 0.
-  // If this condition is true, the JSX code inside the parentheses ((...)) is rendered; otherwise, nothing is rendered.
   return (
     <div className="container mt-5 text-bg-dark p-3 border border-info border-3">
       <h2 className="text-info mb-3">Filter Recipes</h2>
-
-      {/* Form for selecting ingredients */}
       <Form onSubmit={handleSubmit}>
         <div>
           <label className="form-label text-info">Select Ingredients:</label>
@@ -156,29 +106,10 @@ function FilterRecipes() {
           <button type="button" onClick={handleAddField}>
             Add Ingredient
           </button>
-
-          {/* <AddIngredients
-            ingredients={ingredients}
-            inputFields={selectedIngredients}
-            setInputFields={setSelectedIngredients}
-          /> */}
-          {/* <select
-          multiple
-          className="form-control"
-          value={selectedIngredients}
-          onChange={handleIngredientSelect}>
-          {ingredients.map((ingredient, index) => (
-            <option key={index} value={ingredient.name}>
-              {ingredient.name}
-            </option>
-          ))}
-          </select> */}
         </div>
-
-        {/* Button to submit selected ingredients */}
         <button className="btn btn-outline-info mt-3">Filter Recipes</button>
       </Form>
-      {/* Section to display generated recipe */}
+
       {filteredRecipes.length !== 0 && (
         <div className="mt-3">
           <h3>Filtered Recipes</h3>
